e94ec260059e03881bd4ab1567471a784c6d23b1
Taki089Linux <110051322+Taki089Linux@users.noreply.github.com>
2023-03-03 15:12:28 +0700

    ROOT DEFINE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MarkdownModule } from 'ngx-md';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './tutorial/header.component';
-import { FooterComponent } from './tutorial/footer.component';
 
-import { UserComponent } from './example/user.component'; // Add an import to the component pointing to the place where it was created
 import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
@@ -19,13 +16,7 @@ import { ContactComponent } from './contact/contact.component';
     NgbModule.forRoot(),
     MarkdownModule.forRoot(),
   ],
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    UserComponent, // Add the components name to the module declarations,
-    ContactComponent,
-  ],
+  declarations: [AppComponent, ContactComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
