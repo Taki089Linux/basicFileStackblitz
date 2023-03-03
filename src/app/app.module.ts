3a4066a5c51a8d748de1d5c920ec91c25c965c1f
Taki089Linux <110051322+Taki089Linux@users.noreply.github.com>
2023-03-03 15:19:57 +0700

    Complete setting Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MarkdownModule } from 'ngx-md';
 
 import { AppComponent } from './app.component';
 
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ErrorComponent } from './error/error.component';
+import { appRoutes } from './app.routes';
+import { ProductService } from './product.service';
 
 @NgModule({
   imports: [
@@ -15,8 +21,16 @@ import { ContactComponent } from './contact/contact.component';
     FormsModule,
     NgbModule.forRoot(),
     MarkdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
-  declarations: [AppComponent, ContactComponent],
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
+    ProductComponent,
+    ErrorComponent,
+  ],
+  providers: [ProductService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
